a1cba5d72622eeae820f7637060da0c6f9352b55
Lara87 <lara.87@bk.ru>
2023-04-19 22:48:36 +0300

    change

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>Game</h1>
+
+  <section class="game-board">
+    <Card v-for="(card, index) in cardList" :key="`card-${index}`" :value="card"/>
+  </section>
+</template>
+
+<script>
+import Card from './components/Card.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Card,
+  },
+  setup() {
+    const cardList = [];
+
+    for (let i=0; i< 30; i++) {
+      cardList.push(i);
+    }
+
+    return {
+      cardList
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: 100px 100px 100px 100px 100px;
+  grid-template-rows: 100px 100px 100px 100px 100px 100px;
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
+
+.card {
+  border: 10px solid green;
+
+}</style>
